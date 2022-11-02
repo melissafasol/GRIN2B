70cbc066b65d5104e8da198242981e9601112d73
melissafasol <melissaclairefasol@gmail.com>
2022-11-02 16:04:45 +0000

    filter functions for seizures and for normal power calculations

--- filter.py
+++ filter.py
@@ -1,3 +1,4 @@
+from turtle import st
 import scipy
 from scipy.fft import fft, fftfreq
 from scipy import signal
@@ -13,7 +14,8 @@ class Filter:
     high = 100/nyquist
     noise_limit = 3000
     epoch_bins = 1252 #5 seconds * sampling rate
-    
+    seizure_epoch_bins = int(250.4)
+
     def __init__(self, unfiltered_data, timevalues_array):
         self.unfiltered_data = unfiltered_data
         self.timevalues_array = timevalues_array
@@ -22,16 +24,22 @@ class Filter:
         
     '''filter out low-frequency drifts and frequencies above 50Hz'''
     
-    def butter_bandpass(self):
+    def butter_bandpass(self, seizure):
         butter_b, butter_a = signal.butter(self.order, [self.low, self.high], btype='band', analog = False)
         
         filtered_data = signal.filtfilt(butter_b, butter_a, self.unfiltered_data)
 
-        for timevalue in range(len(self.timevalues_array)):
-            start_time_bin = self.timevalues_array[timevalue] 
-            end_time_bin = self.timevalues_array[timevalue] + self.epoch_bins
+        for timevalue in self.timevalues_array:
+            start_time_bin = timevalue
+            if seizure == 'True':
+                end_time_bin = timevalue + self.seizure_epoch_bins
+            else:
+                end_time_bin = timevalue + self.epoch_bins
+            
             self.extracted_datavalues.append(filtered_data[start_time_bin: end_time_bin])
-            
+           
+
+
         for i in range(len(self.extracted_datavalues)):
             for j in range(len(self.extracted_datavalues[i])):
                 if self.extracted_datavalues[i][j] >= self.noise_limit:
@@ -43,23 +51,3 @@ class Filter:
         channels_without_noise = [i for j, i in enumerate(self.extracted_datavalues) if j not in remove_duplicates]
         return channels_without_noise 
 
-
-    #function for seizure epochs that are different lengths and not binned into 5 seconds  
-    def butter_bandpass_seizure(self, seizure_timevalues):
-        butter_b, butter_a = signal.butter(self.order, [self.low, self.high], btype='band', analog = False)
-        
-        filtered_data = signal.filtfilt(butter_b, butter_a, self.unfiltered_data)
-
-        for epoch in seizure_timevalues:
-            self.extracted_datavalues.append(filtered_data[epoch[0]: epoch[1]])
-            
-        for i in range(len(self.extracted_datavalues)):
-            for j in range(len(self.extracted_datavalues[i])):
-                if self.extracted_datavalues[i][j] >= self.noise_limit:
-                    self.channel_threshold.append(i)
-                else:
-                    pass
-
-        remove_duplicates = sorted(list(set(self.channel_threshold)))
-        channels_without_noise = [i for j, i in enumerate(self.extracted_datavalues) if j not in remove_duplicates]
-        return channels_without_noise 
