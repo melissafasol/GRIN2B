18f07e4b643431fcea6cace2dc90e6a145516e7b
melissafasol <melissaclairefasol@gmail.com>
2022-11-28 11:04:50 +0000

    plot by channel region function

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def plot_by_channel(dataframe_to_plot, genotype, sleepstage, save_path, plots_list, plotting_palette):
+def plot_by_animal(dataframe_to_plot, genotype, sleepstage, save_path, plots_list, plotting_palette):
     '''plots list is a list of 5 lists with animal ids'''
     
     def pos_idx_to_animal_idx(row_idx, col_idx):
@@ -18,7 +18,6 @@ def plot_by_channel(dataframe_to_plot, genotype, sleepstage, save_path, plots_li
         for row_idx in range(1):
             for col_idx in range(2):
                 animal_idx = pos_idx_to_animal_idx(row_idx, col_idx)
-                print(animal_idx)
                 animal_data = dataframe_to_plot[dataframe_to_plot["Animal_ID"] == plot[animal_idx]]
                 sns.lineplot(data=animal_data, x='Frequency', y='Power', hue='Channel', ax=axs[col_idx], palette = plotting_palette)
                 #axs[row_idx, col_idx].text(0.5, 0.5, plot[animal_idx], fontsize=12) #test that plt functions are rendering correctly 
@@ -38,4 +37,31 @@ def plot_by_channel(dataframe_to_plot, genotype, sleepstage, save_path, plots_li
         os.chdir(save_path)
         plt.savefig(str(genotype) + '_' + str(sleepstage) + '_' + str(plot) + '.jpg', bbox_inches = 'tight')
 
-   
+
+def plot_by_channel_region(df, sleepstage, save_path):
+    
+    somatosensory = df[df["Channel"].isin([0])] #, 7, 10, 15])]
+    motor = df[df["Channel"].isin([2])] #, 3, 4, 11, 12, 13])]
+    visual = df[df["Channel"].isin([5])] #, 6, 8, 9])]
+    
+    plot_list = [somatosensory, motor, visual]
+    plot_labels = ['Somatosensory', 'Motor', 'Visual']
+    label_list = [0,1, 2]
+    
+    for region_plot, plot_label, label_idx in zip(plot_list,plot_labels, label_list):
+        fig, axs = plt.subplots(1,1, figsize=(15,10), sharex = True, sharey=True)
+        order_plots = ['GRIN2B', 'WT']
+        region_palette = ['orangered', 'teal']
+        sns.lineplot(data=region_plot, x='Frequency', y='Power', hue ='Genotype', hue_order = order_plots, palette = region_palette)
+        plt.suptitle(str(plot_labels[label_idx]) + ' ' + str(sleepstage), fontsize = 30, fontweight = 'bold') 
+        plt.yscale('log')
+        plt.xlim(1, 100)
+        plt.xlabel('Frequency', fontsize=20)
+        plt.ylim(10**-3, 10**5)
+        plt.ylabel(ylabel = 'PSD [V**2/Hz]', fontsize=20)
+        plt.xticks(fontsize=15)
+        plt.yticks(fontsize = 15)
+        os.chdir(save_path)
+        plt.savefig(str(plot_labels[label_idx]) + '_' + str(sleepstage) + '.jpg', bbox_inches = 'tight')
+        plt.show()
+        plt.clf()
