17269356ac8816ed0de9236096e9a49a7d224be9
melissafasol <melissaclairefasol@gmail.com>
2022-12-12 11:11:17 +0000

    class to calculate power on saved idx tracker

--- scripts/idx_tracker.py
+++ scripts/idx_tracker.py
@@ -9,15 +9,16 @@ from filter import Filter
 
 
 class IDX_tracker(Filter):
-    '''Class to track indices of noisy and clean epochs to plot raw data if necessary'''
+    '''Class to track indices of noisy and clean epochs to plot raw data'''
     
-    def __init__(self, brain_state_file, timevalues_array):
+    def __init__(self, unfiltered_data, brain_state_file, timevalues_array):
+        self.unfiltered_data = unfiltered_data
         self.brain_state_file = brain_state_file
         self.timevalues_array = timevalues_array
         self.br_idx = self.brain_state_file.index.values.tolist()
         self.list_br = list(zip(self.br_idx, timevalues_array))
         self.df_index = pd.DataFrame(self.list_br, columns = ['Time_Idx', 'Time_Value'])
-        
+       
 
     def butter_bandpass_index_tracker(self, seizure):
         '''function to keep track of original brain state indices to plot corresponding raw data indices'''
@@ -55,7 +56,7 @@ class IDX_tracker(Filter):
         
         return clean_concat
     
-    def harmonics_algo_idx(filtered_data, save_directory, animal, channel):
+    def harmonics_algo_idx(self, filtered_data, animal, channel):
         def thresholding_algo( y, lag, threshold, influence):
             signals = np.zeros(len(y))
             filteredY = np.array(y)
@@ -93,15 +94,45 @@ class IDX_tracker(Filter):
             i = np.mean(signals[25:50])
             j = np.mean(signals[60:85])
             if intercept > 500 or slope < -5:
-                print('noise artifacts')
+                print('noisy_epochs')
                 noisy_df = pd.DataFrame({'epoch_idx': [key], 'Animal_ID': [animal], 'channel': [channel], 'artifact': ['noise']})
                 noisy_epochs.append(key)
                 noisy_epochs_df.append(noisy_df)
             elif i and j > 0:
-                print('seizure artifacts')
+                print('seizure')
                 noisy_df = pd.DataFrame({'epoch_idx': [key], 'Animal_ID': [animal], 'channel': [channel], 'artifact': ['seizure']})
                 noisy_epochs_df.append(noisy_df)
                 noisy_epochs.append(key)
+            else: 
+                clean_epochs_power.append(power_calculations[1])
+                
+                
+        noisy_indices = [*set(noisy_epochs)]
+    
+        for epoch in sorted(noisy_indices, reverse=True):
+            del filtered_data[epoch]
+        
+        df_psd = pd.DataFrame(clean_epochs_power)
+        mean_values = df_psd.mean(axis = 0)
+        mean_psd = mean_values.to_numpy()
+                
+        return noisy_epochs_df, mean_psd, frequency
+    
+    def power_filtering_idx(self, filtered_data, animal, channel):
+        
+        noisy_epochs_df = []
+        noisy_epochs = []
+        clean_epochs_power = []
+        for key, epoch in filtered_data.items():
+            power_calculations = signal.welch(epoch, window = 'hann', fs = 250.4, nperseg = 1252)
+            frequency = power_calculations[0]
+            slope, intercept = np.polyfit(frequency[0:626], power_calculations[1][0:626], 1)
+        
+            if intercept > 500 or slope < -5:
+                print('noise artifacts')
+                noisy_df = pd.DataFrame({'epoch_idx': [key], 'Animal_ID': [animal], 'channel': [channel], 'artifact': ['noise']})
+                noisy_epochs.append(key)
+                noisy_epochs_df.append(noisy_df)
             else: 
                 print('no artifacts')
                 clean_epochs_power.append(power_calculations[1])
@@ -117,3 +148,73 @@ class IDX_tracker(Filter):
         mean_psd = mean_values.to_numpy()
                 
         return noisy_epochs_df, mean_psd, frequency
+    
+    
+    class HarmonicsPreFiltered():
+        
+        def __init__(self, filtered_data):
+            self.filtered_data = filtered_data
+            
+            
+        def harmonics_algo_idx(self, animal, channel):
+            
+            def thresholding_algo(y, lag, threshold, influence):
+                signals = np.zeros(len(y))
+                filteredY = np.array(y)
+                avgFilter = [0]*len(y)
+                stdFilter = [0]*len(y)
+                avgFilter[lag - 1] = np.mean(y[0:lag])
+                stdFilter[lag - 1] = np.std(y[0:lag])
+                for i in range(lag, len(y)):
+                    if abs(y[i] - avgFilter[i-1]) > threshold * stdFilter [i-1]:
+                        if y[i] > avgFilter[i-1]:
+                            signals[i] = 1
+                        else:
+                            signals[i] = -1
+
+                        filteredY[i] = influence * y[i] + (1 - influence) * filteredY[i-1]
+                        avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+                        stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+                    else:
+                        signals[i] = 0
+                        filteredY[i] = y[i]
+                        avgFilter[i] = np.mean(filteredY[(i-lag+1):i+1])
+                        stdFilter[i] = np.std(filteredY[(i-lag+1):i+1])
+
+                return np.asarray(signals),np.asarray(avgFilter),np.asarray(stdFilter)
+
+
+            noisy_epochs_df = []
+            noisy_epochs = []
+            clean_epochs_power = []
+            for key, epoch in self.filtered_data.items():
+                power_calculations = signal.welch(epoch, window = 'hann', fs = 250.4, nperseg = 1252)
+                frequency = power_calculations[0]
+                slope, intercept = np.polyfit(frequency[0:626], power_calculations[1][0:626], 1)
+                signals, avgfilter, stdfilter = thresholding_algo(y = power_calculations[1], lag = 30, threshold = 5, influence = 0) 
+                i = np.mean(signals[25:50])
+                j = np.mean(signals[60:85])
+                if intercept > 500 or slope < -5:
+                    print('noisy_epochs')
+                    noisy_df = pd.DataFrame({'epoch_idx': [key], 'Animal_ID': [animal], 'channel': [channel], 'artifact': ['noise']})
+                    noisy_epochs.append(key)
+                    noisy_epochs_df.append(noisy_df)
+                elif i and j > 0:
+                    print('seizure')
+                    noisy_df = pd.DataFrame({'epoch_idx': [key], 'Animal_ID': [animal], 'channel': [channel], 'artifact': ['seizure']})
+                    noisy_epochs_df.append(noisy_df)
+                    noisy_epochs.append(key)
+                else: 
+                    clean_epochs_power.append(power_calculations[1])
+                
+                
+            noisy_indices = [*set(noisy_epochs)]
+    
+            for epoch in sorted(noisy_indices, reverse=True):
+                del self.filtered_data[epoch]
+        
+            df_psd = pd.DataFrame(clean_epochs_power)
+            mean_values = df_psd.mean(axis = 0)
+            mean_psd = mean_values.to_numpy()
+                
+            return noisy_epochs_df, mean_psd, frequency
